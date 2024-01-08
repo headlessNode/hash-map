02e32919a113c7aa22c06fe1b6764b82fab7dd70
headlessNode <harriskhan047@outlook.com>
2024-01-08 22:38:28 +0500

    Create Set method - takes in value as an argument, uses hash method to generate a key - key value pair is added to the bucket - add logic to monitor capacity and increase it based on loadFactor

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 class HashMap{
-    constructor(){
-        this.root = [];
+    constructor(initialCapacity, loadFactor){
+        this.buckets = new Array(initialCapacity);
+        this.loadFactor = loadFactor;
+        this.capacity = this.buckets.length;
     }
     hash(value){
         let hashCode = 0;
@@ -8,9 +10,32 @@ class HashMap{
         for(let i = 0; i<value.length; i++){
             hashCode = (hashCode * primeNumber) + value.charCodeAt(i);
         }
-        console.log(hashCode);
+        return hashCode % 16;
+    }
+    set(keyValue){
+        const keyIndex = this.hash(keyValue);
+        if (keyIndex < 0 || keyIndex >= this.capacity) {
+            throw new Error("Trying to access index out of bound");
+        }
+        if(keyIndex > this.capacity * this.loadFactor){
+            this.capacity = this.capacity * 2;
+            this.set(keyValue);
+        }else{
+            this.buckets[keyIndex] = [keyIndex, keyValue];
+        }
     }
 }
 
-let hashmap = new HashMap();
-hashmap.hash('Haris');
+let hashmap = new HashMap(16,0.75);
+hashmap.set('Odin');
+hashmap.set('Thor');
+hashmap.set('Freya');
+hashmap.set('Loki');
+hashmap.set('Frigg');
+hashmap.set('Balder');
+hashmap.set('Heimdall');
+hashmap.set('Fenrir');
+
+
+console.log(hashmap);
+console.log(hashmap.buckets.length);
